2b3a507a8a670c61f6f9bae00da354edb51e4bd6
rigonig <rigonig@pc-rigoni>
2013-02-25 15:01:37 +0100

    Modifiche varie alle funzioni

--- View/Helpers/HTML/Form.php
+++ View/Helpers/HTML/Form.php
@@ -20,7 +20,7 @@ class Form extends Node {
 		parent::__construct($page, 'form');
 		$this->addClass('form-horizontal');
 	}
-	
+
 	/**
 	 * Add a new <input name type value id > element into the form with the passed 
 	 * name and the optional array $option=[labe,id,value].
@@ -34,7 +34,7 @@ class Form extends Node {
 			$controls = $controlGroup->append('<div></div>');
 			$controls->addClass('controls');
 			$label = $controls->append('<label></label>');
-			if (array_key_exists('label', $option)){
+			if (array_key_exists('label', $option)) {
 				$label->text($option['label']);
 			}
 			$label->addClass($method); // $method= radio or checkbox
@@ -53,7 +53,7 @@ class Form extends Node {
 		$input->attr(array('name' => $name, 'type' => $method));
 		if (array_key_exists('value', $option))
 			$input->attr(array('value' => $option['value']));
-		if (array_key_exists('id',$option)) {
+		if (array_key_exists('id', $option)) {
 			$input->id = $option['id'];
 			$label->attr(array('for' => $input->id));
 		}
@@ -71,7 +71,7 @@ class Form extends Node {
 	/**
 	 * Add a new <button type=$type name=$button> with the given name
 	 */
-	public function button($name, $type) {
+	public function button($name, $type, $id = NULL) {
 		$controlGroup = $this->append('<div></div>');
 		$controlGroup->addClass('control-group');
 		$controls = $controlGroup->append('<div></div>');
@@ -79,23 +79,26 @@ class Form extends Node {
 		$btn = $controls->append('<button></button>')->text($name);
 		$btn->addClass('btn');
 		$btn->attr(array('name' => $name, 'type' => $type));
-
+		if ($id)
+			$btn->id = $id;
 		return $btn;
 	}
 
 	/**
 	 * Add a new <textarea rows=$rows> with the number of rows passed to it
 	 */
-	public function textArea($name, $text, $rows = null) {
+	public function textArea($label, $name, $rows = 4, $id = NULL) {
 		$controlGroup = $this->append('<div></div>');
 		$controlGroup->addCLass('control-group');
-		$controlLabel = $controlGroup->append('<label></label>')->text($text);
+		$controlLabel = $controlGroup->append('<label></label>')->text($label);
 		$controlLabel->addClass('control-label');
 		$controls = $controlGroup->append('<div></div>');
 		$controls->addClass('controls');
 		$textarea = $controls->append('<textarea></textarea>')->attr(array('name' => $name, 'rows' => $rows));
-		$textarea->id = $text;
-		$controlLabel->attr(array('for' => $textarea->id));
+		if ($id) {
+			$textarea->id = $id;
+			$controlLabel->attr(array('for' => $textarea->id));
+		}
 		return $textarea;
 	}
 
@@ -120,16 +123,16 @@ class Form extends Node {
 	/**
 	 * Search if an attribute $value exist in $arr 
 	 * replaced by array_key_exist()
-	 */
+	 *
 	public function search($arr = array(), $value) {
-
-		if ($arr) {
-			foreach ($arr as $key => $val) {
-				if ($key == $value)
-					return true;
-			}
-		}
-	}
+	
+	  if ($arr) {
+	    foreach ($arr as $key => $val) {
+	      if ($key == $value)
+	        return true;
+	    }
+	  }
+	}*/
 
 }
 
